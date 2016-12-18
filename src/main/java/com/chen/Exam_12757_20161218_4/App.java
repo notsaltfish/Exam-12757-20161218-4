7ef5b97478c85382705b6e7af853cc4b208f21c2
notsaltedfish <c1097210504@qq.com>
2016-12-18 19:55:44 +0800

    第一次提交

---
+++ src/main/java/com/chen/Exam_12757_20161218_4/App.java
@@ -0,0 +1,13 @@
+package com.chen.Exam_12757_20161218_4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
